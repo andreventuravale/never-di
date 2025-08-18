d55bc4137732b136dadb24ccffbd7dfe0eb90ce3
Andre <andre@datathrust.io>
2025-08-18 13:58:55 -0300

    index.test.ts

--- index.test.ts
+++ index.test.ts
@@ -12,7 +12,7 @@ test("simplest use case", () => {
   expect(sealedContainer.resolve("foo")).toMatchInlineSnapshot(`"foo"`);
 });
 
-test("dependency resolution", () => {
+test("dependency tracking inherently determines the registration order", () => {
   const runtime = DiRuntime();
 
   const container = runtime.createContainer();
@@ -23,6 +23,17 @@ test("dependency resolution", () => {
     return foo + bar;
   }
 
+  // control
+  expect(
+    container
+      .register("foo", () => 1)
+      .register("bar", () => 2)
+      .register("baz", baz)
+      .seal()
+      .resolve("baz")
+  ).toMatchInlineSnapshot(`3`);
+
+  // missing foo and bar
   expect(
     container
       // @ts-expect-error Type '"foo" | "bar"' is not assignable to type 'never'.
@@ -33,6 +44,7 @@ test("dependency resolution", () => {
       .resolve("baz")
   ).toMatchInlineSnapshot(`3`);
 
+  // missing bar
   expect(
     container
       .register("foo", () => 1)
@@ -42,57 +54,4 @@ test("dependency resolution", () => {
       .seal()
       .resolve("baz")
   ).toMatchInlineSnapshot(`3`);
-});
-
-test("dependency tracking inherently determines the registration order", () => {
-  const runtime = DiRuntime();
-
-  const container = runtime.createContainer();
-
-  bar.dependsOn = ["foo"];
-
-  function bar(foo: unknown): unknown {
-    return foo;
-  }
-
-  // control
-  expect(
-    container
-      .register("foo", () => "foo")
-      .register("bar", bar)
-      .seal()
-      .resolve("bar")
-  ).toMatchInlineSnapshot(`"foo"`);
-
-  // sut
-  container
-    .register("bar", (foo) => foo)
-    .register("foo", () => "foo")
-    .seal();
-});
-
-test("circular dependency is detected via seen set", () => {
-  const runtime = DiRuntime();
-
-  const container = runtime.createContainer();
-
-  a.dependsOn = ["b"];
-
-  function a(b: unknown) {
-    return `a(${b})`;
-  }
-
-  b.dependsOn = ["a"];
-
-  function b(a: unknown) {
-    return `b(${a})`;
-  }
-
-  container.register("a", a).register("b", b);
-
-  const sealed = container.seal();
-
-  expect(() => sealed.resolve("a")).toThrowErrorMatchingInlineSnapshot(
-    `[Error: cycle detected: a → b → a]`
-  );
 });
