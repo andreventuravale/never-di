e3f4501f1155b6d93c8b65d65340b0ecade4f457
Andre <andre@datathrust.io>
2025-08-28 19:14:31 -0300

    src/poc.test.ts

--- src/poc.test.ts
+++ src/poc.test.ts
@@ -6,72 +6,102 @@ interface Metadata {
   readonly token?: string;
 }
 
+type WithLazy<S, Token> = S extends {
+  __lazy__: infer Prev;
+}
+  ? { __lazy__: Prev | Token }
+  : { __lazy__: Token };
+
+type WithMeta<S, Meta> = S extends {
+  __meta__: infer Prev;
+}
+  ? { __meta__: Prev & Meta }
+  : { __meta__: Meta };
+
+type WithRegistry<S, F extends Factory> = S extends {
+  __reg__: infer Prev extends Record<string, Factory>;
+}
+  ? { __reg__: Prev | F }
+  : { __reg__: F };
+
+type Tk<M extends Metadata> = M extends { token: infer Tk extends string }
+  ? Tk
+  : never;
+
 interface Factory<T = any, Args extends any[] = any[]> extends Metadata {
   (...args: Args): T;
 }
 
-interface DefineApi<Registry = {}> {
-  defineLazy(f: Factory): Stage2<Registry>;
-  defineLazyMany(f: Factory[]): Stage2<Registry>;
+interface DefineApi<S = {}> {
+  defineLazy<F extends Factory>(f: F): Stage2<WithLazy<S, Tk<F>>>;
+  //   defineLazyMany(f: Factory[]): Stage2<Acc<S, Record<Tk<typeof f>, typeof f>>>;
 }
 
-interface AssignApi<Registry = {}> {
-  assign(f: Factory): Stage3<Registry>;
-  assignMany(f: Factory[]): Stage3<Registry>;
+//--------------------
+
+type Meta<Reg> = Reg extends { __meta__: infer M } ? M : {};
+
+type LazyKeys<Reg> = Reg extends { __lazy__: infer L }
+  ? L extends string
+    ? L
+    : never
+  : never;
+
+type DepKeys<F extends Metadata> = F extends {
+  dependsOn: infer D extends readonly string[];
+}
+  ? D[number]
+  : never;
+
+type UncoveredDeps<Reg, F extends Metadata> = Exclude<
+  DepKeys<F>,
+  keyof Meta<Reg> | LazyKeys<Reg>
+>;
+
+type Check<Reg, F extends Metadata> = [UncoveredDeps<Reg, F>] extends [never]
+  ? Reg
+  : { __unassigned__: UncoveredDeps<Reg, F> };
+
+//--------------------
+
+interface AssignApi<S = {}> {
+  assign<F extends Factory>(
+    f: F
+  ): S extends Check<S, F>
+    ? Stage3<WithRegistry<WithMeta<S, Record<Tk<F>, F>>, F>>
+    : never;
+  //   assignMany(f: Factory[]): Stage3<S>;
 }
 
-interface Stage1<Registry = {}> extends DefineApi<Registry> {}
+interface Stage1<S = {}> extends DefineApi<S> {}
 
-interface Stage2<Registry = {}>
-  extends DefineApi<Registry>,
-    AssignApi<Registry> {}
+interface Stage2<S = {}> extends DefineApi<S>, AssignApi<S> {}
 
-interface Stage3<Registry = {}> extends AssignApi<Registry> {
-  seal(): Container<Registry>;
+interface Stage3<S = {}> extends AssignApi<S> {
+  seal(): Container<S>;
 }
 
-interface Container<Registry = {}> {
-  resolve<T extends keyof Registry>(token: string): Registry[T];
+interface Container<S = {}> {
+  resolve<T extends keyof S>(token: string): S[T];
 }
 
 function createContainerDraft(): Stage1 {
   return {
     defineLazy,
-    defineLazyMany,
-  } satisfies Stage1;
+  } as any;
 
   function defineLazy(f: Factory): Stage2 {
     return {
       assign,
-      assignMany,
       defineLazy,
-      defineLazyMany,
-    } satisfies Stage2;
-  }
-
-  function defineLazyMany(f: Factory[]): Stage2 {
-    return {
-      assign,
-      assignMany,
-      defineLazy,
-      defineLazyMany,
-    } satisfies Stage2;
+    } as any;
   }
 
   function assign(f: Factory): Stage3 {
     return {
       assign,
-      assignMany,
       seal,
-    } satisfies Stage3;
-  }
-
-  function assignMany(f: Factory[]): Stage3 {
-    return {
-      assign,
-      assignMany,
-      seal,
-    } satisfies Stage3;
+    } as Stage3;
   }
 
   function seal(): Container {
